1c4e773496ab3782d746452080728757e0712ece
Andy Armstrong <andy@hexten.net>
2016-06-15 19:47:49 +0100

    Make it work in FF.

--- www/js/iframe-helper.js
+++ www/js/iframe-helper.js
@@ -1,5 +1,32 @@
 $(function() {
+
+  function cb(url) {
+    var p = document.createElement("a");
+    p.href = url;
+    var unique = new Date()
+      .getTime();
+    p.search += (p.search.length ? "&" : "?") + "_cb=" + unique;
+    return p.href;
+  }
+
+  function cbClean(url) {
+    var p = document.createElement("a");
+    p.href = url;
+    if (p.search.length == 0) return p.href;
+    var part = p.search.substr(1)
+      .split("&");
+    var out = [];
+    for (var i = 0; i < part.length; i++) {
+      if (/^_cb=\d+$/.test(part[i])) continue;
+      out.push(part[i]);
+    }
+    p.search = out.length ? ("?" + out.join("&")) : "";
+    console.log("Stripped: " + p.href);
+    return p.href;
+  }
+
   var media = "/media/abcmusicpodcast-august2014.mp3";
+
   if (window === window.top) {
     $("body")
       .replaceWith(
@@ -7,7 +34,7 @@ $(function() {
         .append($("<iframe></iframe>")
           .attr({
             class: "site",
-            src: window.location.href
+            src: cb(window.location.href)
           }))
         .append($("<div></div>")
           .attr({
@@ -18,5 +45,14 @@ $(function() {
               src: media,
               controls: ""
             }))));
+
+    $(".site")
+      .load(function(ev) {
+        console.log("iFrame loaded", ev);
+      });
+  } else {
+    var title = document.title;
+    window.top.history.replaceState({}, title, cbClean(window.location.href));
+    window.top.document.title = title;
   }
 });
--- www/page3.php
+++ www/page3.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>iFrame Monkey Business</title>
+    <title>Page 3</title>
     <link rel="stylesheet" href="/css/main.css" type="text/css" media="all" />
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
     <script type="text/javascript" src="/js/iframe-helper.js"></script>
