fc5659fd93f631c8ea1bf9a125aca471e89dbc5b
Andy Armstrong <andy@hexten.net>
2016-06-15 14:30:31 +0100

    Initial checkin.

---
+++ www/f/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>iFrame Embed</title>
+    <link rel="stylesheet" href="/css/site.css" type="text/css" media="all" />
+  </head>
+  <body>
+    <h1>iFrame embed</h1>
+  </body>
+</html>
+
---
+++ www/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>iFrame Monkey Business</title>
+    <link rel="stylesheet" href="/css/main.css" type="text/css" media="all" />
+  </head>
+  <body>
+    <iframe class="site" src="/f/index.php"></iframe>
+  </body>
+</html>
+
